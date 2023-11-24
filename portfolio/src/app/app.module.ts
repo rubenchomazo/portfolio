202f8ab377d9fe606c7ea1aa5aa20d283a09614d
rubenchomazo <58478880+rubenchomazo@users.noreply.github.com>
2023-11-24 17:32:25 -0500

    Update personal info into portfolio

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WhoamiComponent } from './whoami/whoami.component';
-import { Whoami2Component } from './whoami2/whoami2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WhoamiComponent,
-    Whoami2Component
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, WhoamiComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- portfolio/src/app/whoami/whoami.component.ts
+++ portfolio/src/app/whoami/whoami.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-whoami',
   templateUrl: './whoami.component.html',
-  styleUrls: ['./whoami.component.css']
+  styleUrls: ['./whoami.component.css'],
 })
 export class WhoamiComponent {
+  showMenu: boolean = true;
+  showMoonFill: boolean = false;
+  showJob1: boolean = true;
+  showJob2: boolean = false;
+  showJob3: boolean = false;
 
+  ngOnInit() {
+    localStorage.setItem('color-theme', 'dark');
+    document.documentElement.classList.add('dark');
+  }
+
+  changeMode(mode: string) {
+    if (mode == 'light') {
+      localStorage.setItem('color-theme', 'light');
+      document.documentElement.classList.add('light');
+      document.documentElement.classList.remove('dark');
+      this.showMoonFill = true;
+    } else if (mode == 'dark') {
+      localStorage.setItem('color-theme', 'dark');
+      document.documentElement.classList.add('dark');
+      document.documentElement.classList.remove('light');
+      this.showMoonFill = false;
+    }
+  }
+
+  menuHidden() {
+    const menuItems = document.querySelector('#menu');
+    const menuButtonSpans = document.querySelectorAll('#menu-button span');
+    const links = document.querySelectorAll('#menu a');
+    //Open Menu
+    menuItems?.classList.toggle('hidden');
+    menuButtonSpans.forEach((span) => {
+      span.classList.toggle('animado');
+    });
+    this.showMenu = true;
+  }
+
+  menuShow() {
+    this.showMenu = true;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: any } }) {
+    let w = event.target.innerWidth;
+    if (w > 1024) {
+      this.showMenu = true;
+    } else {
+      this.showMenu = false;
+    }
+  }
+
+  // Job Section
+  changeJob(jobId: string) {
+    let firstLink = document.querySelector('#link1');
+    if (jobId == 'job1') {
+      this.showJob1 = true;
+      this.showJob2 = false;
+      this.showJob3 = false;
+    } else if (jobId == 'job2') {
+      this.showJob1 = false;
+      this.showJob2 = true;
+      this.showJob3 = false;
+      firstLink?.classList.remove(
+        'border-sky-500',
+        'bg-white',
+        'dark:bg-slate-700'
+      );
+    } else if (jobId == 'job3') {
+      this.showJob1 = false;
+      this.showJob2 = false;
+      this.showJob3 = true;
+      firstLink?.classList.remove(
+        'border-sky-500',
+        'bg-white',
+        'dark:bg-slate-700'
+      );
+    }
+  }
 }
